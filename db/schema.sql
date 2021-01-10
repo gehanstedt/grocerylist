114f774d4ee85e8e7906f0139ee646f5c44ce12f
G Dog <greg.hanstedt@med.ge.com>
2021-01-09 19:22:47 -0500

    Adjusted schema.sql to account for changes required for Heroku / JawsDB MySQL.

--- db/schema.sql
+++ db/schema.sql
@@ -15,3 +15,14 @@ CREATE TABLE grocery_list
 -- For Heroku
 --
 USE wi08r4zv8g4aixqw;
+CREATE TABLE grocery_list
+(
+	id int NOT NULL AUTO_INCREMENT,
+	item varchar(255) NOT NULL,
+	in_basket BOOLEAN DEFAULT false,
+	createdAt TIMESTAMP NOT NULL,
+	PRIMARY KEY (id)
+);
+
+ALTER TABLE `wi08r4zv8g4aixqw`.`grocery_list` 
+CHANGE COLUMN `createdAt` `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ;
